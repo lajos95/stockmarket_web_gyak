1865059b57ddaad30af8e78d2d48e3318ee9a039
lajos95 <blali9516@gmail.com>
2026-06-08 15:45:11 +0200

    Update stock selection and display logic for improved functionality

--- stock.js
+++ stock.js
@@ -1,75 +1,38 @@
 const api_key = "d8h7pdhr01qhjpmqvhm0d8h7pdhr01qhjpmqvhmg";
-const symbol = "AAPL";
-const symbol2 = "MSFT";
 
-const url = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${api_key}`;
-const url2 = `https://finnhub.io/api/v1/quote?symbol=${symbol2}&token=${api_key}`;
+let stockName = document.getElementById('stock-name');
+let stockPrice = document.getElementById('stock-price');
+let stockChange = document.getElementById('stock-change');
+let stockChangePercent = document.getElementById('stock-change-percent');
+let stockPreviousClose = document.getElementById('stock-previous-close');
+let stockSelect = document.getElementById('stock-select');
 
-let stockChange = document.getElementById('stock-select');
-
-async function getStockPrice() {
+async function getStockPrice(ticker) {
   try {
+    const url = `https://finnhub.io/api/v1/quote?symbol=${ticker}&token=${api_key}`;
     const response = await fetch(url);
     const data = await response.json();
-    
-    let actualPrice = data.c;
-    let previousClose = data.pc;
-    let priceChange = actualPrice - previousClose;
-    let priceChangePercent = (priceChange / previousClose) * 100;
-    let highPrice = data.h;
-    let lowPrice = data.l;
-    console.log(`Current price of ${symbol}: $${data.c}`);
-    console.log(`Price change: $${priceChange.toFixed(2)} (${priceChangePercent.toFixed(2)}%)`);
-    console.log(`High price of the day: $${highPrice}`);
-    console.log(`Low price of the day: $${lowPrice}`);
-    const applePriceElement = document.getElementById('apple-price');
-    const applePriceChangeElement = document.getElementById('apple-price-change');
-    const applePriceChangePercentElement = document.getElementById('apple-price-change-percent');
-    const applePreviousCloseElement = document.getElementById('apple-previous-close');
-
-    /*if (applePriceElement) {
-      applePriceElement.textContent = actualPrice;
-    }
-    if (applePriceChangeElement) {
-      applePriceChangeElement.textContent = priceChange.toFixed(2);
-    }
-    if (applePriceChangePercentElement) {
-      applePriceChangePercentElement.textContent = priceChangePercent.toFixed(2);
-    }
-    if (applePreviousCloseElement) {
-      applePreviousCloseElement.textContent = previousClose;
-    }*/
+    stockName.textContent = `Stock: ${ticker}`;
+    stockPrice.textContent = `Current Price: $${data.c}`;
+    stockChange.textContent = `Price Change: $${data.d}`;
+    stockChangePercent.textContent = `Price Change Percentage: ${data.dp}%`;
+    stockPreviousClose.textContent = `Previous Close: $${data.pc}`;
   } catch (error) {
     console.error('Error fetching stock price:', error);
   }
 }
 
-stockChange.addEventListener('change', function() {
+stockSelect.addEventListener('change', function () {
   const selectedStockDiv = document.querySelectorAll('.stock-div');
 
-  selectedStockDiv.forEach(div => {
-    div.classList.add('hidden');
-    div.classList.remove('visible');
-  });
-
   const choosenStock = this.value;
-    if (choosenStock) {
-      const selectedDiv = document.getElementById(choosenStock);
-      if (selectedDiv) {
-        selectedDiv.classList.remove('hidden');
-        selectedDiv.classList.add('visible');
-      }
-    }
+  if (choosenStock.value === 0) {
+    document.getElementById('stock-display').textContent = "Válassz egy részvényt...";
+  }
+    /*const selectedDiv = document.getElementById(choosenStock);
+    if (selectedDiv) {
+      selectedDiv.classList.remove('hidden');
+      selectedDiv.classList.add('visible');
+    }*/
+  getStockPrice(choosenStock);
 });
-
-getStockPrice();
-
-/*import { ApiClient, DefaultApi } from 'finnhub';
-
-const api_key = ApiClient.instance.authentications['api_key'];
-api_key.apiKey = "d8h7pdhr01qhjpmqvhm0d8h7pdhr01qhjpmqvhmg"
-const finnhubClient = new DefaultApi()
-
-finnhubClient.quote("AAPL", (error, data, response) => {
-  console.log(data)
-});*/
